62d1e5296d7fcf985667582db5cc67fdd9e7d278
Praveen Kumar <pkkoram98@gmail.com>
2022-02-23 14:58:10 +0530

    add basic keybindings

---
+++ lua/keymaps.lua
@@ -0,0 +1,45 @@
+local keymap = vim.api.nvim_set_keymap;
+local opts = { noremap = true, silent = true }
+
+-- Map <SPACE> as leader key
+vim.g.mapleader = " "
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Move selected line / block of text in visual mode
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Keep visual mode indenting
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Don't yank on delete char
+keymap("n", "x", '"_x', opts)
+keymap("n", "X", '"_X', opts)
+keymap("v", "x", '"_x', opts)
+keymap("v", "X", '"_X', opts)
+
+-- jk or kj for insert mode to normal mode
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+
+--Short fixes
+--Save file by ctrl-s
+keymap("i", "<C-s>", ":w<CR>", opts)
+keymap("n", "<C-s>", "<ESC>:w<CR>", opts)
+
+--Save & quite
+keymap("n", "wq", ":wq<CR>", opts)
+keymap("n", "qq", ":q!<CR>", opts)
