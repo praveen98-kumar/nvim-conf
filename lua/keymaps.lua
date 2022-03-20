81ce30e109bf63017d98fba9b25d0a9d88596bf4
Praveen Kumar <pkkoram98@gmail.com>
2022-03-21 00:37:43 +0530

    add lsp keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -54,3 +54,21 @@ keymap("n", "<S-p>", "<CMD>Telescope live_grep<CR>", { noremap = true })
 
 -- LazyGit
 keymap("n", "<leader>gg", ":LazyGit<CR>", opts)
+
+-- LSP keymaps
+keymap( "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+keymap( "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+keymap( "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+keymap( "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+keymap( "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+keymap( "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+keymap( "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+keymap(
+  "n",
+  "gl",
+  '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>',
+  opts
+)
+keymap( "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+keymap( "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+
